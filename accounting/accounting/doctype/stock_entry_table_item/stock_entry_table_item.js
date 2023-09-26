13a23061086d1c8d2a2fd1fa9828a6f106d4e8b7
Akhil Narang <me@akhilnarang.dev>
2023-09-26 12:19:58 +0530

    refactor: update doctype name
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- accounting/accounting/doctype/stock_entry_table_item/stock_entry_table_item.js
+++ accounting/accounting/doctype/stock_entry_table_item/stock_entry_table_item.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2023, Akhil and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("StockEntryTableItem", {
+// frappe.ui.form.on("Stock Entry Table Item", {
 // 	refresh(frm) {
 
 // 	},
