496c3fcf74fee9dd703b333d99a69dbfa332fcf6
Akhil Narang <me@akhilnarang.dev>
2023-09-27 15:25:34 +0530

    feat: add in stock balance report
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

---
+++ accounting/accounting/report/stock_balance/stock_balance.js
@@ -0,0 +1,41 @@
+// Copyright (c) 2023, Akhil and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Stock Balance"] = {
+    "filters": [
+        {
+            "fieldname": "item",
+            "label": __("Item"),
+            "fieldtype": "Link",
+            "width": "80",
+            "options": "Item",
+            "get_query": function () {
+                return frappe.db.get_list("Item");
+            }
+        },
+        {
+            "fieldname": "warehouse",
+            "label": __("Warehouse"),
+            "fieldtype": "Link",
+            "width": "80",
+            "options": "Warehouse",
+            "get_query": function () {
+                return frappe.db.get_list("Warehouse");
+            }
+        },
+        {
+            "fieldname": "from_date",
+            "label": __("From Date"),
+            "fieldtype": "Datetime",
+            "width": "80",
+            "default": frappe.datetime.add_months(frappe.datetime.now_datetime(), -1),
+        },
+        {
+            "fieldname": "to_date",
+            "label": __("To Date"),
+            "fieldtype": "Datetime",
+            "width": "80",
+            "default": frappe.datetime.now_datetime()
+        }
+    ]
+};
---
+++ accounting/accounting/report/stock_balance/stock_balance.py
@@ -0,0 +1,198 @@
+# Copyright (c) 2023, Akhil and contributors
+# For license information, please see license.txt
+from datetime import datetime
+
+from pydantic import BaseModel
+
+import frappe
+
+
+class Filters(BaseModel):
+    item: str | None = None
+    warehouse: str | None = None
+    from_date: datetime
+    to_date: datetime
+
+
+def execute(incoming_filters: dict) -> tuple[list[dict], list[dict]]:
+    filters = Filters.model_validate(incoming_filters)
+    columns = [
+        {
+            "fieldname": "item",
+            "label": "Item",
+            "fieldtype": "Link",
+            "options": "Item",
+            "width": 200,
+        },
+        {
+            "fieldname": "warehouse",
+            "label": "Warehouse",
+            "fieldtype": "Link",
+            "options": "Warehouse",
+            "width": 200,
+        },
+        {
+            "fieldname": "opening_stock",
+            "label": "Opening Stock",
+            "fieldtype": "Int",
+            "width": 200,
+        },
+        {
+            "fieldname": "incoming_stock",
+            "label": "Incoming Stock",
+            "fieldtype": "Int",
+            "width": 200,
+        },
+        {
+            "fieldname": "outgoing_stock",
+            "label": "Outgoing Stock",
+            "fieldtype": "Int",
+            "width": 200,
+        },
+        {
+            "fieldname": "closing_stock",
+            "label": "Closing Stock",
+            "fieldtype": "Int",
+            "width": 200,
+        },
+        {
+            "fieldname": "valuation_rate",
+            "label": "Valuation Rate",
+            "fieldtype": "Int",
+            "width": 200,
+        },
+    ]
+
+    # Get a combination of unique item-warehouse pairs from the ledger
+    query_string = """
+        SELECT DISTINCT item, warehouse
+        FROM `tabStock Ledger Entry`
+        WHERE entry_time BETWEEN %(from_date)s AND %(to_date)s
+        """
+
+    query_filters = {
+        "from_date": filters.from_date.isoformat(),
+        "to_date": filters.to_date.isoformat(),
+    }
+
+    if filters.item:
+        query_string += " AND item = %(item)s"
+        query_filters["item"] = filters.item
+
+    if filters.warehouse:
+        query_string += " AND warehouse = %(warehouse)s"
+        query_filters["warehouse"] = filters.warehouse
+
+    response: list[dict] = []
+
+    item_warehouse_pairs: list[dict] = frappe.db.sql(
+        query_string, query_filters, as_dict=True
+    )
+    for entry in item_warehouse_pairs:
+        # Get opening stock
+        if opening_stock := frappe.db.sql(
+            """
+            SELECT SUM(quantity) AS result
+            FROM `tabStock Ledger Entry`
+            WHERE entry_time < %(from_date)s
+            AND item = %(item)s
+            AND warehouse = %(warehouse)s
+            """,
+            {
+                "from_date": filters.from_date.isoformat(),
+                "item": entry["item"],
+                "warehouse": entry["warehouse"],
+            },
+            as_dict=True,
+        ):
+            opening_stock = opening_stock[0].get("result", 0)
+
+        # Get incoming stock
+        if incoming_stock := frappe.db.sql(
+            """
+            SELECT SUM(quantity) AS result
+            FROM `tabStock Ledger Entry`
+            WHERE entry_time BETWEEN %(from_date)s AND %(to_date)s
+            AND item = %(item)s
+            AND warehouse = %(warehouse)s
+            AND quantity > 0
+            """,
+            {
+                "from_date": filters.from_date.isoformat(),
+                "to_date": filters.to_date.isoformat(),
+                "item": entry["item"],
+                "warehouse": entry["warehouse"],
+            },
+            as_dict=True,
+        ):
+            incoming_stock = incoming_stock[0].get("result", 0)
+
+        # Get outgoing stock
+        if outgoing_stock := frappe.db.sql(
+            """
+            SELECT SUM(quantity) AS result
+            FROM `tabStock Ledger Entry`
+            WHERE entry_time BETWEEN %(from_date)s AND %(to_date)s
+            AND item = %(item)s
+            AND warehouse = %(warehouse)s
+            AND quantity < 0
+            """,
+            {
+                "from_date": filters.from_date.isoformat(),
+                "to_date": filters.to_date.isoformat(),
+                "item": entry["item"],
+                "warehouse": entry["warehouse"],
+            },
+            as_dict=True,
+        ):
+            outgoing_stock = outgoing_stock[0].get("result", 0)
+
+        # Get closing stock
+        if closing_stock := frappe.db.sql(
+            """
+                SELECT SUM(quantity) AS result
+                FROM `tabStock Ledger Entry`
+                WHERE entry_time <= %(to_date)s
+                AND item = %(item)s
+                AND warehouse = %(warehouse)s
+                """,
+            {
+                "to_date": filters.to_date.isoformat(),
+                "item": entry["item"],
+                "warehouse": entry["warehouse"],
+            },
+            as_dict=True,
+        ):
+            closing_stock = closing_stock[0].get("result", 0)
+
+        # Get valuation rate
+        if valuation_rate := frappe.db.sql(
+            """
+            SELECT AVG(rate) AS result
+            FROM `tabStock Ledger Entry`
+            WHERE entry_time between %(from_date)s AND %(to_date)s
+            AND item = %(item)s
+            AND warehouse = %(warehouse)s
+            """,
+            {
+                "from_date": filters.from_date.isoformat(),
+                "to_date": filters.to_date.isoformat(),
+                "item": entry["item"],
+                "warehouse": entry["warehouse"],
+            },
+            as_dict=True,
+        ):
+            valuation_rate = valuation_rate[0].get("result", 0)
+
+        response.append(
+            {
+                "item": entry["item"],
+                "warehouse": entry["warehouse"],
+                "opening_stock": opening_stock or 0,
+                "incoming_stock": incoming_stock or 0,
+                "outgoing_stock": outgoing_stock or 0,
+                "closing_stock": closing_stock or 0,
+                "valuation_rate": valuation_rate or 0,
+            }
+        )
+    return columns, response
